a0223e1b5a1968fe585aca549254f439b13f0f5c
VladyslavHavshynskyy <havshynskyy@gmail.com>
2021-09-05 16:45:52 +0200

    master

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+import numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def stunden_base():
+    print("Please provide a path to the Stunden_base file")
+    file_path1 = input()[1:-1]
+    Stunden_Base = pd.read_csv(file_path1, sep=';', engine='python')
+    Stunden_Base = Stunden_Base[['DATEWORKED',
+             'Z_MATERIAL',
+             'ACTUAL',
+             'AFK',
+             'EMPLASTNAME',
+             'DAILY_COMMENT',
+             'EMP_SER_NUM',
+             'SERVICE_MONTH']]
+    Stunden_Base['DATEWORKED'] = Stunden_Base['DATEWORKED'].str.replace('.','/', regex=True)
 
+    dummy_num = Stunden_Base[['EMP_SER_NUM']].drop_duplicates()
+    dummy_num['dummy_num'] = np.arange(start=1, stop=len(dummy_num)+1, step=1)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    Stunden_Base = pd.merge(Stunden_Base, dummy_num,
+                          on ='EMP_SER_NUM',
+                          how ='left')
+    return Stunden_Base
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    print(stunden_base())
